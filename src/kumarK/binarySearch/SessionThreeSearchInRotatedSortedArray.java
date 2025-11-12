8b81f8d6b6aed3c2be1d3be014951889b5656de4
tariqmohd643 <tariqmohd643@gmail.com>
2025-11-12 09:35:16 +0530

    binary search

---
+++ src/kumarK/binarySearch/SessionThreeSearchInRotatedSortedArray.java
@@ -0,0 +1,35 @@
+package kumarK.binarySearch;
+
+public class SessionThreeSearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // base case element found
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // left array is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            // right array is sorted
+            else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/kumarK/hashing/question50Onwards/SprinklerOAQuestion54.java
@@ -0,0 +1,7 @@
+package kumarK.hashing.question50Onwards;
+
+public class SprinklerOAQuestion54 {
+
+    // given a string(1-based indexing)
+    //
+}
