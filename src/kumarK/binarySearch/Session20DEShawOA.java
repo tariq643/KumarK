48f6b364eaf38f5e4593002c9fa4d4da2aa4ec18
tariqmohd643 <tariqmohd643@gmail.com>
2025-11-22 09:47:39 +0530

    binary search

---
+++ src/kumarK/binarySearch/Session20DEShawOA.java
@@ -0,0 +1,33 @@
+package kumarK.binarySearch;
+
+public class Session20DEShawOA {
+
+   /* We are given “n” type of items ; array b is given which tells us ->
+    b[i] = frequency of item “i” ;
+
+    Our task is to make a group of k distinct items;
+
+    And finally output how many maximum such types of groups can be formed.
+
+    [3 3 3] ; K = 2
+    Output :- 4
+
+    Observation : if you want to make y groups of size 'k': then sum of the array >= y*k
+
+    Question is to find the maximum 'y'
+
+    Brute force :
+
+    first try for y = 1
+    if it is possible for y = 1
+    then try for y = 2;
+    if yest try for y = 3 and so on .....
+
+
+
+    */
+
+
+
+}
+
