10a4af9f78dcea2eb177bde70d0523d0ed1a33d8
tariqmohd643 <tariqmohd643@gmail.com>
2025-12-13 18:56:07 +0530

    binary search

---
+++ src/kumarK/dynamicProgramming/Session2GeneralPattern.java
@@ -0,0 +1,16 @@
+package kumarK.dynamicProgramming;
+
+/*
+
+    1) dp[] -> N
+
+    2) find dp[1], dp[2] and dp[3] by basic observation
+
+    3) create a general formual for dp[i]
+
+
+ */
+
+
+public class Session2GeneralPattern {
+}
