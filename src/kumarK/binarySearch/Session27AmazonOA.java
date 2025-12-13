50bdf4e86e1dbace2d7171a1b520eaacdae75e3b
tariqmohd643 <tariqmohd643@gmail.com>
2025-12-13 18:04:53 +0530

    binary search

--- src/kumarK/binarySearch/Session27AmazonOA.java
+++ src/kumarK/binarySearch/Session27AmazonOA.java
@@ -51,6 +51,56 @@ Once b[k] is decided ; how do you know if it will satisfy all the conditions?
 
 Min possible case :-> b[k] = y ; (k+1…..n) :->(y-1…y-2……….)
 
+For each value from 1 to M ; for b[k] = i ; you can know the lowest and maximum sum possible.
+
+If b[k] = i ; how will you know if this is a valid array ?
+Max_Sum and Min_sum ;
+Min_sum<=M<=Max_sum → array b is valid.
+
+-> If you know the max sum of array and min sum of array ; then it means -> if m is lying b/w [min sum , max sum] -> valid hence i is valid answer .
+
+Brute Force : - O(N*M)
+
+Code:- https://ideone.com/4VGu9j (replace true by 1)
+Java->  https://ideone.com/f1b9na
+Py:-> https://ideone.com/IpyiMw
+
+Efficient :- Efficient Solution :- Let's say y is the largest number possible at index ‘k’ .  You can find the y using binary search as it a monotonic function ; suppose you have a function f(i) ;
+
+G(i) = true —> it means for x = i ; we can be successful
+
+Hence f array will always look like this :-
+
+
+FFFFFFFFFFFFFFFFFT T T T T T T T T T T T T T T T T T T T F F F F F F F F F F F
+
+Hence there will be some F in the starting and at some particular point T will start coming and it will keep coming then F will keep coming forever
+
+
+Say you are doing a binary search :-
+1                                                                            M
+[F F F F F F F F T T F F F F F F F F F F F F F F F F]
+
+Low = 1
+High = N
+Mid = (low+mid)/2 ;
+G[mid] = false ;
+B[K] = mid
+But the question is how do you identify if this the
+last part of array or first part of the array
+
+For B[k] = mid ; you can calculate the max sum and min sum ;
+If M>max sum ; then first part
+If M<min_sum ; then last part
+
+Code :- https://ideone.com/ffltqm
+Java :-> https://ideone.com/DFhfuc
+Python :-> https://ideone.com/9CkxSE
+
+-> Bonus you can solve this problem in O(LogN)
+
+
+
 */
 
 
