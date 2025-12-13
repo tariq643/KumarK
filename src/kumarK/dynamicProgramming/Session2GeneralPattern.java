acb1166e812b16592bb19f681091e6bd51ce187d
tariqmohd643 <tariqmohd643@gmail.com>
2025-12-13 19:51:52 +0530

    binary search

--- src/kumarK/dynamicProgramming/Session2GeneralPattern.java
+++ src/kumarK/dynamicProgramming/Session2GeneralPattern.java
@@ -8,9 +8,27 @@ package kumarK.dynamicProgramming;
 
     3) create a general formual for dp[i]
 
+    4) dp[n] will be the final answer
 
  */
 
+
+/*
+    find the maximum sum subset such that no two elements in the array are adjacent
+
+    dp[1] -> best answer to the problem when the size of array was 1
+    dp[2] -> best answer to the problem when the size of array was 2
+    dp[3] -> best answer to the problem when the size of array was 3
+
+    ... example if the array is [2,-3,5,-8,7]
+
+    so dp[1] = 2
+    dp[2] = 2
+    dp[3] = 7
+
+    dp[i] = Math.max(dp[i-1] , arr[i] + dp[i-2])
+
+ */
 
 public class Session2GeneralPattern {
 }
