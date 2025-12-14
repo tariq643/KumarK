37b45760c152befb8a437c36bceee160526b2698
tariqmohd643 <tariqmohd643@gmail.com>
2025-12-14 17:39:09 +0530

    dp

---
+++ src/kumarK/dynamicProgramming/Session9AtCodeDP.java
@@ -0,0 +1,54 @@
+package kumarK.dynamicProgramming;
+
+/*
+
+Taro's summer vacation starts tomorrow, and he has decided to make plans for it now.
+
+The vacation consists of
+N days. For each
+i (
+1≤i≤N), Taro will choose one of the following activities and do it on the
+i-th day:
+
+A: Swim in the sea. Gain
+a
+i
+​
+  points of happiness.
+B: Catch bugs in the mountains. Gain
+b
+i
+​
+  points of happiness.
+C: Do homework at home. Gain
+c
+i
+​
+  points of happiness.
+As Taro gets bored easily, he cannot do the same activities for two or more consecutive days.
+
+Find the maximum possible total points of happiness that Taro gains.
+ */
+
+
+/*
+
+    Greedy vs Dynamic Programming
+    two pointer is greedy algorithm
+
+    if a problem can be solved by greedy approach
+    it means that it is guaranteed that at each step
+    you make a choice and that choice is best
+    for now as well as the future ...
+    then the problem can be solved using greedy method
+
+    if there are multiple choice at each steps and it is not
+    clear which choice will give better result long term
+    then use dynamic programming
+
+ */
+
+public class Session9AtCodeDP {
+
+
+}
