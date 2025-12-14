1d0b9ed0ad4037c752e556af48c70e82faeee32c
tariqmohd643 <tariqmohd643@gmail.com>
2025-12-14 13:01:44 +0530

    binary search

--- src/kumarK/dynamicProgramming/Session6PaypalOA.java
+++ src/kumarK/dynamicProgramming/Session6PaypalOA.java
@@ -26,4 +26,28 @@ Py https://ideone.com/h2gnlv
  */
 
 public class Session6PaypalOA {
+
+    private int lengthOfLongestValidSubstring (String str, int k) {
+
+        int length = str.length(), maxLength = 1;
+        int dp[] = new int[length];
+        dp[0] = 1;
+        for (int i = 1; i < length; i++) {
+            if (Math.abs(str.charAt(i) - str.charAt(i - 1)) <= k) {
+                dp[i] = dp[i - 1] + 1;
+            }
+            else {
+                dp[i] = 1;
+            }
+            if (dp[i] > maxLength) {
+                maxLength = dp[i];
+            }
+        }
+        return maxLength;
+    }
+
+    static void main() {
+
+    }
+
 }
