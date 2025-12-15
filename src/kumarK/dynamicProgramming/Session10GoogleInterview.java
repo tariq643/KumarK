ade86b1950d1180f9fbdcfeb8ba7854120ea34bb
tariqmohd643 <tariqmohd643@gmail.com>
2025-12-15 15:00:50 +0530

    dp

---
+++ src/kumarK/dynamicProgramming/Session10GoogleInterview.java
@@ -0,0 +1,103 @@
+package kumarK.dynamicProgramming;
+
+/*
+
+    Link to the question :-> https://www.desiqna.in/13822/google-dp-interview-question-jan-2023
+Find the number of journeys where you start from index 1 and end at index ‘n’ and the sum of every journey should be odd..
+
+Allowed to make jumps of size 1 or 2
+5 4 2 6
+
+Number of odd sum journey :
+-> (5,2,6)
+-> (5,4,2,6)
+-> (5,4,6)
+
+Number of even sum journey :
+-> Nothing
+
+
+2 3 5 8 10
+
+Evn sum journey 👍
+//evn
+    //2...3...5...8...10
+    //2...3...5.....10
+
+Odd sum journey :
+
+
+    //odd
+    //2...5...10
+    //2...3...8...10
+    //2...5...8....10
+
+
+
+
+
+Dp[i][even] = number of even sum journey till index ‘i’
+Dp[i][odd] = number of odd sum journey till index ‘i’
+
+
+
+
+->
+[1 2 3 4]
+
+Dp[3][even] = dp[1][odd] + dp[2][odd]
+
+
+
+
+
+If a[i] is even ….
+
+Dp[i][eve] = dp[i-1][eve] + dp[i-2][even]
+
+Dp[i][odd] = dp[i-1][odd] + dp[i-2][odd]
+
+
+If a[i] is odd …..
+
+Dp[i][even] = dp[i-1][odd] + dp[i-2][odd]
+
+
+Dp[i][odd] = dp[i-1][eve] + dp[i-2][eve]
+C++ https://ideone.com/kqxkFB
+Java https://ideone.com/f5sswM
+Py https://ideone.com/Yt6SLb
+
+
+
+
+
+
+Find the number of journeys where you start from index 1 and end at index ‘n’ and the sum of every journey should be odd/even..
+
+Allowed to make jumps of size 1 or 2 or
+3.
+
+If a[i] is even ….
+
+Dp[i][even] = dp[i-1][even] + dp[i-2][even] + dp[i-3][even]
+
+Dp[i][odd] = dp[i-1][odd] + dp[i-2][odd] + dp[i-3][odd]
+
+
+If a[i] is odd …..
+
+Dp[i][even] = dp[i-1][odd] + dp[i-2][odd] + dp[i-3][odd]
+
+
+Dp[i][odd] = dp[i-1][even] + dp[i-2][even] + dp[i-3][even]
+
+C++ https://ideone.com/Zafite
+Java https://ideone.com/cEV9RN
+Py https://ideone.com/mDK3UE
+
+
+ */
+
+public class Session10GoogleInterview {
+}
--- src/kumarK/dynamicProgramming/Session9AtCodeDP.java
+++ src/kumarK/dynamicProgramming/Session9AtCodeDP.java
@@ -178,12 +178,79 @@ public class Session9AtCodeDP {
         future will be great of final answer will be maximum
 
      */
-    private int session9AtCodeDynamicProgramming (int n, int a[], int b[],int c[]) {
+    /*
+        3 arrays a b and c hence we create 3 arrays
 
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum = sum + Math.max(a[i], Math.max(b[i], c[i]));
+        dpa[i] = best answer to the question if the size of the
+        array was 'i' and you selected a[i] at the 'i' th index
+
+        dpb[i] =  best answer to the question if the size of the
+        array was 'i' and you selected b[i] at the 'i' th index
+
+        dpc[i] =  best answer to the question if the size of the
+        array was 'i' and you selected c[i] at the 'i' th index
+
+
+        a1 b1 c1
+        a2 b2 c2
+        . . . .
+        ......
+
+
+
+        ......
+
+        dpa[1] = a1
+        dpb[1] = b1
+        dpc[1] = c1
+
+        now dpa[2] = a2 + either the element b1 or c1
+        so  dpa[2] = a2 + max(b1, c1)
+
+        similarly dpb[2] = b2 + either the element a1 or c1
+        so  dpb[2] = b2 + max(a1, c1)
+
+        similarly dpc[2] = c2 + either the element b1 or c1
+        so  dpc[2] = c2 + max(a1, b1)
+
+        so in general ....
+        dpa[i] = a[i] + max(dpb[i-1],dbc[i-1]);
+        dpb[i] = b[i] + max(dpa[i-1],dbb[i-1]);
+     */
+
+    private long session9AtCodeDynamicProgramming (int n, long a[], long b[],long c[]) {
+
+        long[] dpa = new long[n];
+        long[] dpb = new long[n];
+        long[] dpc = new long[n];
+
+        dpa[0] = a[0];
+        dpb[0] = b[0];
+        dpc[0] = c[0];
+
+        if (n > 1) {
+            dpa[1] = a[1] + Math.max(dpb[0], dpc[0]);
+            dpb[1] = a[1] + Math.max(dpa[0], dpc[0]);
+            dpc[1] = a[1] + Math.max(dpa[0], dpb[0]);
         }
-        return sum;
+
+        for (int i = 2; i < n; i++) {
+            dpa[i] = a[i] + Math.max(dpb[i - 1], dpc[i - 1]);
+            dpb[i] = b[i] + Math.max(dpa[i - 1], dpc[i - 1]);
+            dpc[i] = c[i] + Math.max(dpa[i - 1], dpb[i - 1]);
+        }
+        long result = Math.max(dpa[n - 1], Math.max(dpb[n - 1], dpc[n - 1]));
+        return result;
+    }
+
+    static void main() {
+        Session9AtCodeDP obj = new Session9AtCodeDP();
+        long a[] = {10, 20 ,30};
+        long b[] = {40, 50 ,60};
+        long c[] = {70, 80 ,90};
+        /*long a[] = {10 ,40, 70};
+        long b[] = {20 ,50 ,80};
+        long c[] = {30 ,60 ,90};*/
+        System.out.println(obj.session9AtCodeDynamicProgramming(3,a,b,c));
     }
 }
